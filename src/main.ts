61799f0dc35f4d96185aa05ce1b5826efe1e5d01
Vinicius Martins <vinima@microsoft.com>
2020-08-24 17:01:39 -0600

    add initial version of action

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,46 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const token = core.getInput('github-token', { required: true });
+    const pattern = core.getInput('pattern', { required: true });
+    const group = core.getInput('group', { required: true })
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const labels = await listLabelsOnIssue(token);
+    core.info("Found " + labels.length + " labels")
 
-    core.setOutput('time', new Date().toTimeString())
+    let substrings = extractTextFromLabels(labels, new RegExp(pattern), Number(group))
+
+    core.setOutput("substrings", substrings);
   } catch (error) {
-    core.setFailed(error.message)
+    core.setFailed(error.message);
   }
+}
+
+async function listLabelsOnIssue(token: string) {
+  // Fetch the list of labels attached to the issue that
+  // triggered the workflow
+  let client = github.getOctokit(token);
+
+  const opts = client.issues.listLabelsOnIssue.endpoint.merge({
+    ...github.context.repo,
+    issue_number: github.context.issue.number
+  });
+
+  return await client.paginate<any>(opts);
+}
+
+function extractTextFromLabels(labels: any[], pattern: RegExp, index: number) {
+  let substrings = []
+  for (const label of labels) {
+    var match = label.name.match(pattern);
+    if (match && index <= match.length - 1 && substrings.findIndex(match[index]) > 0) {
+      substrings.push(match[index]);
+    }
+  }
+
+  return substrings;
 }
 
 run()
